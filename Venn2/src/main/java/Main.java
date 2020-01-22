d2d62153724cfc03a390a6da1c1b852ac570fb56
devesh-ramnarine <devesh12@my.yorku.ca>
2020-01-22 13:39:53 -0500

    Building window and button

--- Venn2/src/main/java/Main.java
+++ Venn2/src/main/java/Main.java
@@ -1,8 +1,38 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class Main {
+
+
+
+public class Main extends Application{
+	
+	Button button;
 
 	public static void main(String[] args) {
-		System.out.println(":)");
+launch(args);
+
 	}
+	
+	public void start(Stage primaryStage) throws Exception{
+		
+		primaryStage.setTitle("title of window");
+		button = new Button();
+		button.setText("Click me");
+		
+		StackPane layout = new StackPane();
+		layout.getChildren().add(button);
+		
+		Scene scene = new Scene(layout,300,250);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+			
+		
+	}
+	
 
 }
