5636c761b27eade26200abb50dfb0daa6718c2fc
devesh-ramnarine <devesh12@my.yorku.ca>
2020-01-22 19:23:59 -0500

    diagram circles

--- Venn2/src/main/java/CircleExample.java
+++ Venn2/src/main/java/CircleExample.java
@@ -1,13 +1,34 @@
-import javafx.application.Application; 
-import javafx.scene.Group; 
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage; 
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 import javafx.stage.*;      
 import javafx.scene.shape.Circle;
 import javafx.scene.paint.Color;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+
+import javafx.stage.Stage;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 
 public class CircleExample extends Application { 
@@ -16,18 +37,20 @@ public class CircleExample extends Application {
 	public void start(Stage primaryStage) {
 		
 		
-	    Pane pane = new Pane();
-	    Circle circle1 = new Circle();
-	    Circle circle2 = new Circle();
-
-
-	      
+		
+		
+		Pane pane = new Pane();		
 	    Scene scene = new Scene(pane, 1200, 800);
-	  
-     
-	    circle1.setCenterX(1200/3);
-	    circle1.setCenterY(800/2);
+	    
+	    
+	    Circle circle1 = new Circle();
+	    Circle circle2 = new Circle();  
 
+	   
+	  
+	    circle1.setCenterX(pane.getWidth()/3);
+	    circle1.setCenterY(800/2);
+	    
 	    
 	    circle1.setRadius(350);
 	    circle1.setStroke(Color.RED);
@@ -37,21 +60,32 @@ public class CircleExample extends Application {
 	   
 	    circle2.setCenterX(800);
 	    circle2.setCenterY(800/2);
+	    
+	    
+	    
 	    circle2.setRadius(350);
 	    circle2.setStroke(Color.BLUE);
 	    circle2.setFill(null);
 	    circle2.setStrokeWidth(5);
 	    
 	    
-	 
-	    pane.getChildren().addAll(circle1, circle2);
+	    
+	    pane.getChildren().addAll(circle1,circle2);
+	    
+	    TextField userTextField = new TextField();
+        userTextField.setPrefWidth(800);
+        primaryStage.setScene(new Scene(userTextField));
+        primaryStage.show();
 
 	  
 	    primaryStage.setTitle("ShowCircle");
 	    primaryStage.setScene(scene);
 	    primaryStage.show();
+	    
+	    
 	}
 
+	
 	public static void main(String[] args) {
 	    launch(args);
 	}
